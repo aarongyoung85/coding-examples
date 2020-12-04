ebd95c15cc41a64e855d3d4c97edbffd56e423a3
Aaron Young <kalamity@kalamity-MBP.local>
2020-12-04 02:41:00 -0800

    fixing formatting

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 Basic coding examples with technical comments. The code presented here is not working code.
 It's purpose is to show coding patterns and design and the thought process behind those decisions.
-
-After committing to github and looking over the files, the formatting is really off. I apologize :(
--- http_handler_example.go
+++ http_handler_example.go
@@ -6,15 +6,15 @@ Usually, this file would be broken up into separate files/packages.
 package examplePackage
 
 import (
-	"context"
-	"fmt"
-	"net/http"
+    "context"
+    "fmt"
+    "net/http"
     "database/sql"
     errs "errors"
 
     "github.com/private-repo/negotiate"
     "github.com/private-repo/settings"
-	"github.com/sirupsen/logrus"
+    "github.com/sirupsen/logrus"
     "gihub.com/husobee/vestigo"
     "github.com/pkg/errors"
 )
@@ -70,53 +70,52 @@ func main() {
 //   receivers should be pointers to avoid confusion.
 // i'll explain why these are pointers shortly
 func (c *Controller) InitializeUserSettings() error {
-        // notice here I don't instantiate the variable as a pointer like i did in main().
-		usd := userSettingsData{}
+    // notice here I don't instantiate the variable as a pointer like i did in main().
+    usd := userSettingsData{}
 
-        // but here, I explicitly pass a pointer to c.SettingsClient.Get 
-		if err := c.settingsClient.Get(&usd); err != nil {
-            // first example of using sentinel errors in Golang's error wrapping.
-			return fmt.Errorf("failed to get user settings. %s. %w", err, errInternal)
-		}
+    // but here, I explicitly pass a pointer to c.SettingsClient.Get 
+    if err := c.settingsClient.Get(&usd); err != nil {
+        // first example of using sentinel errors in Golang's error wrapping.
+        return fmt.Errorf("failed to get user settings. %s. %w", err, errInternal)
+    }
 
-        // a lot of Golang code instantiates a pointer when the variable is created.
-        // i prefer instantiating as a value and EXPLICITLY passing a pointer when needed.
-        // i believe this pattern of programming encourages functional-style programming
-        //   which is more explicit, easier to test, easier to maintain, and easier to read
+    // a lot of Golang code instantiates a pointer when the variable is created.
+    // i prefer instantiating as a value and EXPLICITLY passing a pointer when needed.
+    // i believe this pattern of programming encourages functional-style programming
+    //   which is more explicit, easier to test, easier to maintain, and easier to read
 
-        // because i modify the Controller struct here, i need the method receiver to be a pointer.
-        // if the method receiver was a value, this line of code will only live for the life of this function.
-        // i want every function that has the same receiver to have the modified data.
-		c.settingsData = usd
-	}
+    // because i modify the Controller struct here, i need the method receiver to be a pointer.
+    // if the method receiver was a value, this line of code will only live for the life of this function.
+    // i want every function that has the same receiver to have the modified data.
+    c.settingsData = usd
 
 	return nil
 }
 
 // POST /v1/update-settings
 func (c *Controller) UpdateUserSettingsHandler(rw http.ResponseWriter, req *http.Request) {
-	n := negotiate.GetNegotiator(req)
+    n := negotiate.GetNegotiator(req)
 
     // here, you truly see why the method receiver is a pointer.
     // because of this handler, i can update the service's settings whenever i want
     //   by simply curling the endpoint.
     // since the method receiver is a pointer, all functions will get the updated settings.
-	if err := c.InitializeUserSettings(); err != nil {
-		logrus.WithError(err).Error("failed to update user settings")
-		n.Respond(rw, http.StatusInternalServerError, response.Error(nil))
-		return
-	}
+    if err := c.InitializeUserSettings(); err != nil {
+        logrus.WithError(err).Error("failed to update user settings")
+        n.Respond(rw, http.StatusInternalServerError, response.Error(nil))
+        return
+    }
 
     // return c.SettingsData to see what the updated settings are
-	n.Respond(rw, http.StatusOK, response.Success(c.SettingsData))
+    n.Respond(rw, http.StatusOK, response.Success(c.SettingsData))
 }
 
 // POST /v1/user
 func (c *Controller) CreateUserHandler(rw http.ResponseWriter, req *http.Request) {
     // Golang's use of context is an area of much debate and i discuss it in context_package_example.go.
-	ctx := req.Context()
+    ctx := req.Context()
     lf := logrus.Fields{"handler": "CreateUser"}
-	n := negotiate.GetNegotiator(req)
+    n := negotiate.GetNegotiator(req)
 
     if !c.SettingsData.Enabled {
         // could argue this could return different statuses.
@@ -131,21 +130,21 @@ func (c *Controller) CreateUserHandler(rw http.ResponseWriter, req *http.Request
     // this makes the code easier to maintain because anyone can look at one handler and
     //   instantly understand what to expect.
     // i leverage Golang's error wrapping to communicate to the main handler what the status should be.
-	userResp, err := c.handleCreateUser(ctx, req)
-	lf["user_id"] = user.ID
-	if err != nil {
+    userResp, err := c.handleCreateUser(ctx, req)
+    lf["user_id"] = user.ID
+    if err != nil {
         logrus.WithFields(lf).WithError(err).Error("failed to create user")
 
         // Golang's new (go1.13) way of dealing with errors.
-		if errs.Is(err, errBadRequest) {
+        if errs.Is(err, errBadRequest) {
             // return the error so the client can fix it.
-			n.Respond(rw, http.StatusBadRequest, response.Error(err))
-		} else if errs.Is (err, errInternal){
+            n.Respond(rw, http.StatusBadRequest, response.Error(err))
+        } else if errs.Is (err, errInternal){
             // don't want the client to know about internal errors.
-			n.Respond(rw, http.StatusInternalServerError, response.Error(nil))
-		}
-		return
-	}
+            n.Respond(rw, http.StatusInternalServerError, response.Error(nil))
+        }
+        return
+    }
 
     n.Respond(rw, http.StatusCreated, response.Success(userResp))
 }
@@ -170,32 +169,32 @@ func (c *Controller) handleCreateUser(ctx context.Context, req *http.Request) (c
     //   and decreases the performance of your application.
     // TLDR: returning pointers adds memory to the heap that the garbage collector has to track and clean up.
     // returning values keeps the memory on the stack along with the function, which is much more efficient.
-	resp := createUserResponse{}
+    resp := createUserResponse{}
 
-	cur := createUserRequest{}
+    cur := createUserRequest{}
     // again, explicitly declare a pointer when necessary (&cur).
-	if err := json.NewDecoder(req.Body).Decode(&cur); err != nil {
+    if err := json.NewDecoder(req.Body).Decode(&cur); err != nil {
         // use the %w directive and use a sentinel error, which gets interpreted to an http response code at the
         //   main handler level.
         // this function doesn't need to know about http response codes.
         return resp, fmt.Errorf("failed to decode. %s. %w", err, errBadRequest)
-	}
+    }
 
     // this function doesn't modify "cur" so it doesn't need it to be a pointer.
     // ie. this function won't produce any side effects
-	if err := validateCreateUserRequest(cur); err != nil {
-		return resp, fmt.Errorf("failed to validate create user request. %s. %w", err, errBadRequest)
-	}
+    if err := validateCreateUserRequest(cur); err != nil {
+        return resp, fmt.Errorf("failed to validate create user request. %s. %w", err, errBadRequest)
+    }
 
     // didn't bother writing this function out.
-	userID, err := c.DB.InsertUser(ctx, cur)
-	if err != nil {
+    userID, err := c.DB.InsertUser(ctx, cur)
+    if err != nil {
         // if something went wrong, it had to have been an internal server error level of error.
-		return resp, fmt.Errorf("failed to insert user. %s. %w", err, errInternal)
-	}
+        return resp, fmt.Errorf("failed to insert user. %s. %w", err, errInternal)
+    }
 
     resp.ID = userID
-	return resp, nil
+    return resp, nil
 }
 
 func validateCreateUserRequest(cur CreateUserRequest) error {
@@ -203,7 +202,7 @@ func validateCreateUserRequest(cur CreateUserRequest) error {
     // that means at this moment, "errs" is an empty slice, as you would expect.
     // BUT it can accept a maximum of 5 strings before it needs to allocate a new slice with greater capacity.
     // this is an optimization technique.
-	errs := make([]string, 0, 5)
+    errs := make([]string, 0, 5)
 
     // i could say the same thing using a literal: 
     // errs := []string{}
@@ -216,29 +215,29 @@ func validateCreateUserRequest(cur CreateUserRequest) error {
     // since i already know the maximum bound of the slice, i declare it when i make the slice.
     // this avoids extra allocations and improves performance.
 
-	if cur.FullName == "" {
-		errs = append(errs, "full name is required")
-	}
+    if cur.FullName == "" {
+        errs = append(errs, "full name is required")
+    }
 
     if cur.Address == "" {
-		errs = append(errs, "address is required")
-	}
+        errs = append(errs, "address is required")
+    }
 
     if cur.City == "" {
-		errs = append(errs, "city is required")
-	}
+        errs = append(errs, "city is required")
+    }
 
     if cur.State == "" || len(cur.State) != 2 {
-		errs = append(errs, "state is required and must be 2 characters")
-	}
+        errs = append(errs, "state is required and must be 2 characters")
+    }
 
     if cur.ZipCode == 0 {
-		errs = append(errs, "zip code is required")
-	}
+        errs = append(errs, "zip code is required")
+    }
 
-	if len(errs) > 0 {
-		return fmt.Errorf("%s", strings.Join(errs, "; "))
-	}
+    if len(errs) > 0 {
+        return fmt.Errorf("%s", strings.Join(errs, "; "))
+    }
 
-	return nil
+    return nil
 }
